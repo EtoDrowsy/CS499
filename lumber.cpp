9b5f9f5c02c3c62f7fabf2d97491516ae4b28da0
Silivek <wkilpatrick90@gmail.com>
2025-01-17 20:36:31 -0600

    Added Lumber superclass and child classes
    Required variables and constructors added, lumber dimensions fetch added

---
+++ lumber.cpp
@@ -0,0 +1,75 @@
+#include <string>
+
+class Lumber {
+    private:
+        std::string location;
+        std::string species;
+        float length;
+        float width;
+        float thickness;
+    public:
+        Lumber() {
+            location = "NULL";
+            species = "NULL";
+            length = 0;
+            width = 0;
+            thickness = 0;
+        }
+        Lumber(std::string l, std::string s, float le, float w, float t) {
+            location = l;
+            species = s;
+            length = le;
+            width = w;
+            thickness = t;
+        }
+        float* getDimensions() {
+            float dimensions[3] = {length, width, thickness};
+            return dimensions;
+        }
+};
+
+class Piece : Lumber {
+    private:
+        std::string serialNumber;
+        std::string surface;
+        std::string dried;
+    public:
+        Piece() 
+        : Lumber() {
+            serialNumber = "0000";
+            surface = "NULL";
+            dried = "NULL";
+        }
+        Piece(std::string l, std::string s, float le, float w, float t, std::string se, std::string su, std::string d) 
+        : Lumber(l, s, le, w, t) {
+            serialNumber = se;
+            surface = su;
+            dried = d;
+        }
+};
+
+class Bulk : Lumber {
+    private:
+        std::string catalogNumber;
+        int quantity;
+        std::string units;
+        std::string description;
+        float price;
+    public:
+        Bulk() 
+        : Lumber() {
+            catalogNumber = "0000";
+            quantity = 0;
+            units = "NULL";
+            description = "NULL";
+            price = 0;
+        }
+        Bulk(std::string l, std::string s, float le, float w, float t, std::string c, int q, std::string u, std::string d, float p) 
+        : Lumber(l, s, le, w, t) {
+            catalogNumber = c;
+            quantity = q;
+            units = u;
+            description = d;
+            price = p;
+        }
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "lumber.h"
+#include "lumber.cpp"
 
 int main() {
 	return 0;
