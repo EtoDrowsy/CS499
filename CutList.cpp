b90b7a606e1f8edab5e1a06b6e28159f3579830b
Silivek <wkilpatrick90@gmail.com>
2025-04-22 16:45:13 -0500

    confirm cut list return refactor
    returns a struct with a vector of IDs and a vector of Lumber* objects

--- CutList.cpp
+++ CutList.cpp
@@ -108,24 +108,32 @@ static std::vector <std::vector <int>> cutListToLumberList(std::vector <Lumber*>
     }
     return lumberList;
 }
+struct confirmer {
+    std::vector <std::string> IDs;
+    std::vector <Lumber*> newLumber;
+};
 
-static std::vector <Lumber*> confirmLumberList(std::vector <Lumber*> inventory, std::vector <CutListItem*> cutList, std::vector <std::vector <int>> lumberList) {
-    std::vector <Lumber*> newInventory = inventory;
+static confirmer confirmLumberList(std::vector <Lumber*> inventory, std::vector <CutListItem*> cutList, std::vector <std::vector <int>> lumberList) {
+    std::vector <std::string> IDs;
+    std::vector <Lumber*> newLumber;
     for (int i = 0; i < lumberList.size(); i++) {
         for (int j : lumberList.at(i)) {
             float leftovers = inventory.at(j)->getLength() - SAW_KERF - cutList.at(i)->getLength();
+            IDs.push_back(inventory.at(j)->getID());
             if (leftovers < 0)
                 continue;
             leftovers = int(std::floor(leftovers));
-            newInventory.at(j)->setQuantity(newInventory.at(j)->getQuantity() - 1);
-            Lumber* newLumber = new Lumber(inventory.at(j)->getAttributes(), inventory.at(j)->getAttributeValues());
-            newLumber->setLength(leftovers);
-            newLumber->setDescription("Piece");
-            newLumber->setQuantity(1);
+            Lumber* newWood = new Lumber(inventory.at(j)->getAttributes(), inventory.at(j)->getAttributeValues());
+            newWood->setLength(leftovers);
+            newWood->setDescription("Piece");
+            newWood->setQuantity(1);
             float fPrice = std::floor((leftovers / inventory.at(j)->getLength() * inventory.at(j)->getPrice()) * 100) / 100;
-            newLumber->setPrice(fPrice);
-            newInventory.push_back(newLumber);
+            newWood->setPrice(fPrice);
+            newLumber.push_back(newWood);
         }
     }
-    return newInventory;
+    confirmer returnee;
+    returnee.IDs = IDs;
+    returnee.newLumber = newLumber;
+    return returnee;
 }
